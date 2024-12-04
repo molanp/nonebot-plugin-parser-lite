aef282fe9cb640efb1b4846b777467ae748bfa66
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-04 21:58:31 +0800

    chore: 优化小红书解析逻辑

--- nonebot_plugin_resolver2/matchers/xiaohongshu.py
+++ nonebot_plugin_resolver2/matchers/xiaohongshu.py
@@ -15,36 +15,29 @@ from ..config import *
 # 小红书下载链接
 XHS_REQ_LINK = "https://www.xiaohongshu.com/explore/"
 
-
 xiaohongshu = on_keyword(keywords={"xiaohongshu.com", "xhslink.com"}, rule = Rule(is_not_in_disable_group))
 
 @xiaohongshu.handle()
 async def _(bot: Bot, event: MessageEvent):
-
     message: str = event.message.extract_plain_text().replace("&amp;", "&").strip()
-
-    msg_url = re.search(r"(http:|https:)\/\/(xhslink|(www\.)xiaohongshu).com\/[A-Za-z\d._?%&+\-=\/#@]*",message)[0]
-    # 如果没有设置xhs的ck就结束，因为获取不到
-    xhs_ck = rconfig.r_xhs_ck
-    if xhs_ck == "":
-        await xiaohongshu.finish(f"{NICKNAME}解析 | 小红书 - 无法获取到管理员设置的小红书 cookie！")
+    if match := re.search(r"(http:|https:)\/\/(xhslink|(www\.)xiaohongshu).com\/[A-Za-z\d._?%&+\-=\/#@]*",message):
+        msg_url = match.group(0)
     # 请求头
     headers = {
                   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
                             'application/signed-exchange;v=b3;q=0.9',
-                  'cookie': xhs_ck,
+                  'cookie': rconfig.r_xhs_ck,
               } | COMMON_HEADER
     if "xhslink" in msg_url:
         async with httpx.AsyncClient() as client:
             resp = await client.get(msg_url, headers=headers, follow_redirects=True)
-            msg_url = str(resp.url)
-    xhs_id = re.search(r'/explore/(\w+)', msg_url)
-    if not xhs_id:
-        xhs_id = re.search(r'/discovery/item/(\w+)', msg_url)
-    if not xhs_id:
-        xhs_id = re.search(r'source=note&noteId=(\w+)', msg_url)
-    xhs_id = xhs_id[1]
-
+            msg_url = resp.url
+    # ?: 非捕获组
+    pattern = r'(?:/explore/|/discovery/item/|source=note&noteId=)(\w+)'
+    if match := re.search(pattern, msg_url):
+        xhs_id = match.group(1)
+    else:
+        return
     # 解析 URL 参数
     parsed_url = urlparse(msg_url)
     params = parse_qs(parsed_url.query)
@@ -54,16 +47,14 @@ async def _(bot: Bot, event: MessageEvent):
     async with httpx.AsyncClient() as client:
         resp = await client.get(f'{XHS_REQ_LINK}{xhs_id}?xsec_source={xsec_source}&xsec_token={xsec_token}', headers=headers)
         html = resp.text
-    # response_json = re.findall('window.__INITIAL_STATE__=(.*?)</script>', html)[0]
-    try:
-        response_json = re.findall('window.__INITIAL_STATE__=(.*?)</script>', html)[0]
-    except IndexError:
-        await xiaohongshu.send(
-            Message(f"{NICKNAME}解析 | 小红书 - 当前ck已失效，请联系管理员重新设置的小红书 cookie！"))
-        return
-    response_json = response_json.replace("undefined", "null")
-    response_json = json.loads(response_json)
-    note_data = response_json['note']['noteDetailMap'][xhs_id]['note']
+    pattern = r'window.__INITIAL_STATE__=(.*?)</script>'
+    if match := re.search(pattern, html):
+        json_str = match.group(1)
+    else:
+        await xiaohongshu.finish("小红书 cookie 可能已失效")
+    json_str = json_str.replace("undefined", "null")
+    json_obj = json.loads(json_str)
+    note_data = json_obj['note']['noteDetailMap'][xhs_id]['note']
     type = note_data['type']
     note_title = note_data['title']
     note_desc = note_data['desc']
@@ -82,7 +73,6 @@ async def _(bot: Bot, event: MessageEvent):
     elif type == 'video':
         # 这是一条解析有水印的视频
         logger.info(note_data['video'])
-
         video_url = note_data['video']['media']['stream']['h264'][0]['masterUrl']
 
         # ⚠️ 废弃，解析无水印视频video.consumer.originVideoKey
