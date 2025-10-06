5d459aa66e57fd062404c49f4c9540dbdd6ed3d3
Les Freire <fllessive@gmail.com>
2025-10-06 17:46:27 +0800

    ✨ feat: improve data extraction for twitter parser (#274)
    * ✨ feat: improve data extraction for twitter parser
    
    * 🔧 chore: update codecov coverage target from 40% to 20%

--- src/nonebot_plugin_parser/parsers/twitter.py
+++ src/nonebot_plugin_parser/parsers/twitter.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import re
 from typing import Any, ClassVar
 
@@ -73,41 +74,42 @@ class TwitterParser(BaseParser):
         from .data import ParseData
 
         soup = BeautifulSoup(html_content, "html.parser")
-        data = ParseData()
+        data = ParseData(name="无用户名")
+
+        def get_tag_href(tag) -> str | None:
+            if isinstance(tag, Tag):
+                if href := tag.get("href"):
+                    return str(href)
+            return None
 
         # 1. 提取缩略图链接
-        img_tag = soup.find("img")
-        if img_tag and isinstance(img_tag, Tag):
-            src = img_tag.get("src")
-            if src and isinstance(src, str):
-                data.cover_url = src
+        thumb_tag = soup.find("img")
+        if thumb_tag:
+            data.cover_url = get_tag_href(thumb_tag)
 
         # 2. 提取下载链接
-        download_links = soup.find_all("a", class_="tw-button-dl")
-        # class="abutton is-success is-fullwidth  btn-premium mt-3"
-        download_items = soup.find_all("a", class_="abutton")
-        for link in download_links + download_items:
-            if isinstance(link, Tag) and (href := link.get("href")) and isinstance(href, str):
-                href = href
-            else:
+        tw_button_tags = soup.find_all("a", class_="tw-button-dl")
+        abutton_tags = soup.find_all("a", class_="abutton")
+        for tag in chain(tw_button_tags, abutton_tags):
+            href = get_tag_href(tag)
+            if href is None:
                 continue
-            text = link.get_text(strip=True)
 
-            if "下载图片" in text:
-                # 从图片下载链接中提取原始图片URL
-                data.images_urls.append(href)
-            elif "下载 gif" in text:
-                data.dynamic_urls.append(href)  # GIF和MP4是同一个文件
-            elif "下载 MP4" in text:
-                # 从GIF/MP4下载链接中提取原始视频URL
+            text = tag.get_text(strip=True)
+            if "下载 MP4" in text:
                 data.video_url = href
                 break
+            elif "下载图片" in text:
+                data.images_urls.append(href)
+            elif "下载 gif" in text:
+                data.dynamic_urls.append(href)
 
         # 3. 提取标题
         title_tag = soup.find("h3")
         if title_tag:
             data.title = title_tag.get_text(strip=True)
 
+        return data
         # # 4. 提取Twitter ID
         # twitter_id_input = soup.find("input", {"id": "TwitterId"})
         # if (
@@ -116,6 +118,3 @@ class TwitterParser(BaseParser):
         #     and (value := twitter_id_input.get("value"))
         #     and isinstance(value, str)
         # ):
-        data.name = "暂时无法获取用户名"
-
-        return data
---
+++ tests/parsers/test_twitter.py
@@ -0,0 +1,78 @@
+import asyncio
+import re
+
+from nonebot import logger
+
+
+async def test_video():
+    from nonebot_plugin_parser.parsers import TwitterParser
+
+    parser = TwitterParser()
+
+    urls = [
+        "https://x.com/Fortnite/status/1904171341735178552",
+    ]
+
+    async def parse_video(url: str):
+        matched = next(re.search(pattern, url) for _, pattern in parser.patterns)
+        assert matched, "无法匹配 URL"
+        logger.info(f"{url} | 开始解析推特视频")
+        result = await parser.parse(matched)
+        logger.debug(f"{url} | 解析结果: \n{result}")
+        assert result.title, "标题为空"
+        video_contents = result.video_contents
+        assert video_contents, "视频内容为空"
+        for video_content in video_contents:
+            path = await video_content.get_path()
+            assert path.exists(), "视频不存在"
+
+    await asyncio.gather(*[parse_video(url) for url in urls])
+
+
+async def test_img():
+    from nonebot_plugin_parser.parsers import TwitterParser
+
+    parser = TwitterParser()
+
+    urls = [
+        "https://x.com/Fortnite/status/1870484479980052921",  # 单图
+        "https://x.com/chitose_yoshino/status/1841416254810378314",  # 多图
+    ]
+
+    async def parse_img(url: str):
+        matched = next(re.search(pattern, url) for _, pattern in parser.patterns)
+        assert matched, "无法匹配 URL"
+        logger.info(f"{url} | 开始解析推特图片")
+        result = await parser.parse(matched)
+        logger.debug(f"{url} | 解析结果: \n{result}")
+        img_contents = result.img_contents
+        assert img_contents, "图片内容为空"
+        for img_content in img_contents:
+            path = await img_content.get_path()
+            assert path.exists(), "图片不存在"
+
+    await asyncio.gather(*[parse_img(url) for url in urls])
+
+
+async def test_gif():
+    from nonebot_plugin_parser.parsers import TwitterParser
+
+    parser = TwitterParser()
+
+    urls = [
+        "https://x.com/Dithmenos9/status/1966798448499286345",
+    ]
+
+    async def parse_gif(url: str):
+        matched = next(re.search(pattern, url) for _, pattern in parser.patterns)
+        assert matched, "无法匹配 URL"
+        logger.info(f"{url} | 开始解析推特 GIF")
+        result = await parser.parse(matched)
+        logger.debug(f"{url} | 解析结果: \n{result}")
+        gif_contents = result.dynamic_contents
+        assert gif_contents, "GIF 内容为空"
+        for gif_content in gif_contents:
+            path = await gif_content.get_path()
+            assert path.exists(), "GIF 不存在"
+
+    await asyncio.gather(*[parse_gif(url) for url in urls])
