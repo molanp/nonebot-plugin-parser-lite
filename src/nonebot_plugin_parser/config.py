a041bdc49aa2c85504235ab59b44b26210efcc6e
Les Freire <64878354+fllesser@users.noreply.github.com>
2025-12-20 17:15:59 +0800

    feat: add HTML renderer with Jinja2 templating and associated tests. (#419)

--- src/nonebot_plugin_parser/config.py
+++ src/nonebot_plugin_parser/config.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from pathlib import Path
 
 from nonebot import require, get_driver, get_plugin_config
@@ -6,7 +5,7 @@ from apilmoji import ELK_SH_CDN, EmojiStyle
 from pydantic import BaseModel
 from bilibili_api.video import VideoCodecs, VideoQuality
 
-from .constants import PlatformEnum
+from .constants import RenderType, PlatformEnum
 
 require("nonebot_plugin_localstore")
 import nonebot_plugin_localstore as _store
@@ -14,12 +13,6 @@ import nonebot_plugin_localstore as _store
 _cache_dir: Path = _store.get_plugin_cache_dir()
 _config_dir: Path = _store.get_plugin_config_dir()
 _data_dir: Path = _store.get_plugin_data_dir()
-
-
-class RenderType(str, Enum):
-    default = "default"
-    common = "common"
-    htmlkit = "htmlkit"
 
 
 class Config(BaseModel):
