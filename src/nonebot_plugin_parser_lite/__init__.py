75feccc36eacfe98d694eabdc3745c2b4f5ac976
stla <130404897+Stlara-F@users.noreply.github.com>
2026-07-28 12:57:50 +0800

    feat: support PARSER_LITE_STANDALONE env var

--- src/nonebot_plugin_parser_lite/__init__.py
+++ src/nonebot_plugin_parser_lite/__init__.py
@@ -1,22 +1,19 @@
 import os
+from nonebot import logger, require
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
 
-_STANDALONE: bool = bool(os.environ.get("PARSER_LITE_STANDALONE"))
+_STANDALONE = bool(os.environ.get("PARSER_LITE_STANDALONE"))
 
-# ═══ 修改: 仅在非独立模式下 require 依赖插件 ═══
-if _STANDALONE:
-    # 独立模式: 全部 NoneBot 插件可选
-    _Scheduler = None
-else:
-    from nonebot import logger, require
-    from nonebot.plugin import PluginMetadata, inherit_supported_adapters
-
+if not _STANDALONE:
     require("nonebot_plugin_alconna")
     require("nonebot_plugin_uninfo")
     require("nonebot_plugin_htmlrender")
     require("nonebot_plugin_apscheduler")
     require("nonebot_plugin_localstore")
 
-    from nonebot_plugin_apscheduler import scheduler as _Scheduler
+    from nonebot_plugin_apscheduler import scheduler
+else:
+    scheduler = None
 
 from .config import Config
 
@@ -26,35 +23,36 @@ if not _STANDALONE:
 
 from .utils.cache import CacheManager
 
-if _STANDALONE:
-    __plugin_meta__ = None
-else:
-    __plugin_meta__ = PluginMetadata(
-        name="链接分享解析 Lite 版",
-        description="通用媒体链接分享解析",
-        usage="发送支持平台的(BV号/链接/小程序/卡片)即可",
-        type="application",
-        homepage="https://github.com/sokoko-org/nonebot-plugin-parser-lite",
-        config=Config,
-        supported_adapters=inherit_supported_adapters(
-            "nonebot_plugin_alconna", "nonebot_plugin_uninfo"
-        ),
-        extra={
-            "author": "molanp",
-            "homepage": "https://github.com/sokoko-org/nonebot-plugin-parser-lite",
-            "version": "1.3.0",
-            "plugin_type": "NORMAL",
-        },
-    )
+__plugin_meta__ = PluginMetadata(
+    name="链接分享解析 Lite 版",
+    description="通用媒体链接分享解析",
+    usage="发送支持平台的(BV号/链接/小程序/卡片)即可",
+    type="application",
+    homepage="https://github.com/sokoko-org/nonebot-plugin-parser-lite",
+    config=Config,
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_alconna", "nonebot_plugin_uninfo"
+    ),
+    extra={
+        "author": "molanp",
+        "homepage": "https://github.com/sokoko-org/nonebot-plugin-parser-lite",
+        "version": "1.3.1",
+        "plugin_type": "NORMAL",
+    },
+) if not _STANDALONE else None
 
 
-if _Scheduler is not None:
-    @_Scheduler.scheduled_job("interval", hours=2, id="parser-clean-local-cache")
+if scheduler is not None:
+
+    @scheduler.scheduled_job("interval", hours=2, id="parser-clean-local-cache")
     async def clean_plugin_cache() -> None:
+        """周期性清理过期缓存文件，并重置解析状态。"""
+
         try:
             await CacheManager.clean_expired()
         except Exception as e:
             logger.exception(f"清理缓存文件时发生异常: {e!r}")
+
         if not _STANDALONE:
             clear_result_cache()
             BrowserManager.clear_cache()
